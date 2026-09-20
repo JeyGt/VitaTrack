06f2068b6935be36975319eb5798527503a26eb7
JeyGt <jguiot77@gmail.com>
2026-09-20 21:40:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'vitatrack-sport-custom-v1';
-const CACHE_VERSION = 'v137-body-composition';
+const CACHE_VERSION = 'v138-body-composition-deployfix';
 const CORE_CACHE = `${CACHE_PREFIX}core-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${CACHE_VERSION}`;
 
